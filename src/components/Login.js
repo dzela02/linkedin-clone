e4cd3c237577e0622e9f4788d0d7a942f281a467
dzela02 <62295694+dzela02@users.noreply.github.com>
2021-05-09 23:38:44 +0200

    Front-end finished ADD: friebase&redux 🚀

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { connect } from "react-redux";
 
 const Login = (props) => {
     return (
@@ -167,6 +168,12 @@ const Google = styled.button`
     }    
 `;
 
+const mapStateToProps = (state) => {
+    return {};
+};
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({});
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login);
 
