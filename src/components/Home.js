27b03a34a2a7bc4a0d674620425b8b26ab6f65a3
dzela02 <62295694+dzela02@users.noreply.github.com>
2021-05-09 14:27:23 +0200

    left main right of home added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import Main from './Main'
+import Leftside from './Leftside'
+import Rightside from './Rightside'
 
 function Home() {
     return (
@@ -10,6 +13,11 @@ function Home() {
                     Find talented pros in record time with Upwork!
                 </p>
             </Section>
+            <Layout>
+                <Leftside />
+                <Main />
+                <Rightside />
+            </Layout>
         </Container>
     )
 };
@@ -49,5 +57,20 @@ const Section = styled.section`
         padding: 0 5px;
     }
 `;
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
 
 export default Home
---
+++ src/components/Rightside.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Rightside() {
+    return (
+        <Container>
+            Right side
+        </Container>
+    )
+}
+const Container = styled.div`
+    grid-area: rightside;
+`;
+
+export default Rightside;
